16ee4f48d8d9d0d1961d78f795f9efe62b329d6a
Koen Vossen <koen@teamtv.nl>
2021-01-03 21:34:58 +0100

    Update

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,64 +1,77 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
+const Label = ({title, children}) => {
+  return (
+    <div className="flex">
+      <div className="w-1/2 md:w-2/5">{title}:</div>
+      <div className="w-1/2 md:w-3/5 font-medium">{children}</div>
+    </div>
+  );
+};
+
+const Card = ({}) => {
+  return (
+    <div className="p-10 m-4 bg-white rounded-lg">
+        <div className="sm:flex items-end">
+            <img src="https://opensource.pysport.org/img/python.png"
+            width="100" height="100" className="mx-auto md:mx-0" />
+            <div className="md:pt-6 md:px-8 text-left space-y-4 h-full">
+                <figcaption>
+                  <div class="font-bold text-4xl align-middle pt-3">
+                    kloppy
+                  </div>
+                  <div class="text-blue-400 text-xl font-bold pt-3">
+                    IO Python package
+                  </div>
+                </figcaption>
+            </div>
+        </div>
+        <div className="pt-2 mt-8">
+          <div className="grid grid-cols-1 md:grid-cols-2">
+            <Label title="Sports">Soccer</Label>
+            <Label title="Platforms">Python</Label>
+            <Label title="Author">koenvo</Label>
+            <Label title="License">MIT</Label>
+          </div>
+        </div>
+    </div>
+  );
+};
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="bg-gray-200 h-full">
+        <style global jsx>{`
+      html,
+      body,
+      body > div:first-child,
+      div#__next,
+      div#__next > div,
+      div#__next > div > div {
+        min-height: 100vh;
+      }
+    `}</style>
+
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main>
+           <div className="container mx-auto">
+           <div className="grid grid-cols-1 lg:grid-cols-2">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            </div>
+            </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+      <footer>
       </footer>
     </div>
   )
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./pages/**/*.js', './components/**/*.js'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      spacing: {
+        '1/2': '50%'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
